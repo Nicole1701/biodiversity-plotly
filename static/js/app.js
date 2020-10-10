3c8b721f6206f5410cb8b4a5cde05fd2cc7853d8
misspengy <64616410+misspengy@users.noreply.github.com>
2020-10-09 19:29:46 -0700

    updating code

--- static/js/app.js
+++ static/js/app.js
@@ -1,24 +1,24 @@
-// Fetch the JSON data and console log it
-d3.json("data/samples.json").then((data) => {
-  console.log(data);
-});
-
 /**
  * Helper function to select stock data
  * Returns an array of values
  * @param {array} rows
  * @param {integer} index
- * index 0 - Id
- * index 1 - Ethnicity
- * index 2 - Gender
- * index 3 - Age
- * index 4 - Location
- * index 5 - BB Type
- * index 6 - Wfreq
+ * index 0 - id
+ * index 1 - otu_ids
+ * index 2 - sample_values
+ * index 3 - otu_labels
+
  */
 
-function unpack(rows, index) {
-  return rows.map(function (row) {
-    return row[index];
-  });
-}
+// Fetch the JSON data and console log it
+let bbdata = d3.json("data/samples.json").then((bbdata) => {
+  console.log(bbdata);
+  var ids = bbdata.samples[0].id;
+  console.log(ids);
+  var otu = bbdata.samples[1].otu_ids;
+  console.log(otu);
+  var values = bbdata.samples[2].sample_values;
+  console.log(values);
+  var labels = bbdata.samples[3].otu_labels;
+  console.log(labels);
+});
