5b3ece87e6197120e17a05bb9db4c98a24ef725a
misspengy <64616410+misspengy@users.noreply.github.com>
2020-10-09 17:44:16 -0700

    code update

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,24 @@
-// Pull in Json file
+// Fetch the JSON data and console log it
 d3.json("data/samples.json").then((data) => {
   console.log(data);
 });
+
+/**
+ * Helper function to select stock data
+ * Returns an array of values
+ * @param {array} rows
+ * @param {integer} index
+ * index 0 - Id
+ * index 1 - Ethnicity
+ * index 2 - Gender
+ * index 3 - Age
+ * index 4 - Location
+ * index 5 - BB Type
+ * index 6 - Wfreq
+ */
+
+function unpack(rows, index) {
+  return rows.map(function (row) {
+    return row[index];
+  });
+}
