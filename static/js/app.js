fcd73685986c47709d0fabca3ae360abd9b254ae
misspengy <64616410+misspengy@users.noreply.github.com>
2020-10-10 22:34:07 -0700

    adding hbar

--- static/js/app.js
+++ static/js/app.js
@@ -11,7 +11,9 @@ function init() {
       dropdownMenu.append("option").text(name).property("value");
     });
 
+    //Dislay data for charts/tables on the page
     demoInfo(bbData.names[0]);
+    buildPlot(bbData.names[0]);
   });
 }
 
@@ -21,87 +23,50 @@ function demoInfo(subject) {
   d3.json("data/samples.json").then((bbData) => {
     console.log(bbData.metadata);
 
-    //Build the Demo Table
-    let demo = bbData.metadata.filter(
-      (item) => item.id.toString() == subject
-    )[0];
+    // Filter metadata for display
+    let demo = bbData.metadata.filter((item) => item.id.toString() == subject);
     console.log(demo);
 
-    // let demoResult = demo[0];
-    // console.log(demoResult);
+    // Identify the result to display
+    let demoResult = demo[0];
+    console.log(demoResult);
 
+    // Use D3 to select the demo table
     let demoTable = d3.select("#sample-metadata");
 
     //Clear any previous data from the table
     demoTable.html("");
 
-    Object.entries(demo).forEach((key) => {
-      demoTable.append("h5").text(key[0].toUpperCase() + ": " + key[1]);
+    // Append the data to the data
+    Object.entries(demoResult).forEach(([key, value]) => {
+      demoTable.append("h5").text(key.toUpperCase() + ": " + value);
     });
   });
 }
 
+// Create horizontal bar chart
+/**
+ * Helper function to select data
+ * Returns an array of values
+ * @param {array} rows
+ * @param {integer} index
+ * index 0 - id
+ * index 1 - otu_ids
+ * index 2 - sample_values
+ * index 3 - otu_labels
+ */
+
+function buildPlot(subject) {
+  // Fetch the JSON data and console log it
+  d3.json("data/samples.json").then((bbData) => {
+    console.log(bbData.samples);
+  });
+}
+
+// Change event function
 function optionChanged(subject) {
   demoInfo(subject);
+  buildPlot(subject);
 }
 
 init();
-
-// /**
-//  * Helper function to select data
-//  * Returns an array of values
-//  * @param {array} rows
-//  * @param {integer} index
-//  * index 0 - id
-//  * index 1 - otu_ids
-//  * index 2 - sample_values
-//  * index 3 - otu_labels
-
-//  */
-// d3.json("data/samples.json").then((data) => {
-//   console.log(data);
-
-//   function buildPlot(bar) {
-//     // Fetch the JSON data and console log it
-
-//     var ids = data.samples[0].id;
-//     console.log(ids);
-//     var otu = data.samples[0].otu_ids;
-//     console.log(otu);
-//     var values = sampledata.samples[0].sample_values;
-//     console.log(values);
-//     var labels = bdata.samples[0].otu_labels;
-//     console.log(labels);
-//   }
-
-//   var trace1 = {
-//     type: "bar",
-//     x: values,
-//     y: otu,
-//   };
-
-//   var data = [trace1];
-
-//   var layout = {
-//     title: `${id} OTU ID's`,
-//     xaxis: { title: "OTU ID" },
-//     yaxis: { title: "Sample Values" },
-//   };
-
-//   Plotly.newPlot("bar", data, layout);
-
-//   buildPlot(bar);
-// });
-
-// Fetch the JSON data and console log it
-// let bbdata = d3.json("data/samples.json").then((bbdata) => {
-//   console.log(bbdata);
-//   var ids = bbdata.samples[0].id;
-//   console.log(ids);
-//   var otu = bbdata.samples[1].otu_ids;
-//   console.log(otu);
-//   var values = bbdata.samples[2].sample_values;
-//   console.log(values);
-//   var labels = bbdata.samples[3].otu_labels;
-//   console.log(labels);
-// });
